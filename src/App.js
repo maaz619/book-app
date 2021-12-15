256cce54c10d82fdd57ddd090e71ecde7447c718
maaz619 <maazahmedlfc@gmail.com>
2021-12-16 01:18:12 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Good <span className='header-text'>read</span></h1>
       </header>
+      <div className="buttons">
+        <button className="buttons-type">Programming</button>
+        <button className="buttons-type">Biography</button>
+        <button className="buttons-type">Fictional</button>
+        <button className="buttons-type">Comics</button>
+      </div>
+      <main className="book-list-container">
+        <ul>
+          <h1 style={{ "textAlign": "start", "padding": "1rem" }} className='header-text'>Programming</h1>
+          <li className='book-details'><h2>book name</h2></li>
+          <li className='book-details'><h2>book name</h2></li>
+          <li className='book-details'><h2>book name</h2></li>
+          <li className='book-details'><h2>book name</h2></li>
+        </ul>
+      </main>
     </div>
   );
 }
