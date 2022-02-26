f2b6c39b816399ec821c99ddbc718415ecbc2cf4
maaz619 <maazahmedlfc@gmail.com>
2022-02-26 13:01:24 +0530

    user authentication success

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import Home from "./components/Home";
 import Login from "./components/Login/Login";
 import Order from "./components/Order";
 import Product from "./components/Product";
@@ -7,6 +6,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SignUp from "./components/Login/Signup";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Home from "components/Home";
 
 const App = () => {
   return (
